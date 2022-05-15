850ccd525da9eb962439887253035c9e7c5cf915
julioribeiro1 <juliodevstudent@gmail.com>
2022-05-15 10:48:17 -0300

    if em javascript

---
+++ exe008.js
@@ -0,0 +1,6 @@
+var vel = 68
+console.log(`Sua velocidade é de ${vel}km/h `)
+if (vel > 60){
+    console.log(`Sua velocidade é de ${vel}km/h e voçê acima da velocidade permitida.`)
+}
+console.log('Diriga com cuidade!!')
