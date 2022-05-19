98291b16ab5346cb2d16336d879a973be66ccdce
julioribeiro1 <juliodevstudent@gmail.com>
2022-05-19 09:57:54 -0300

    Estruturas de repetições

---
+++ repetoções/estruturas_de_repetições.js
@@ -0,0 +1,17 @@
+/*var c = 1 
+while(c <= 5) {
+    console.log('Olá tudo bem?')
+    c++
+}
+
+var c = 1 
+do{
+    console.log('Olá tudo bem?')
+    c++
+}while(c <= 5) 
+*/
+
+for (var c= 1; c <= 5; c++){
+    console.log('vai começar')
+}
+console.log('Fimm')
