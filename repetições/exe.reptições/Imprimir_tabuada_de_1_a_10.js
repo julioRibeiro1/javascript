7395c71adfa5d82ec390ee583d5c6cc647a30f68
julioribeiro1 <juliodevstudent@gmail.com>
2022-05-24 09:26:45 -0300

    Imorimir tabuada de 1 a 10

---
+++ repetições/exe.reptições/Imprimir_tabuada_de_1_a_10.js
@@ -0,0 +1,10 @@
+function imprimirtabuada (n){
+    for(let i = 1; i<11; i++){
+        let linha = n + ' * ' + i + ' = ' + n * i
+            console.log ( linha)
+    }
+}
+        for (let i = 1; i<11;i++){
+            imprimirtabuada(i)
+               console.log ('')
+}
