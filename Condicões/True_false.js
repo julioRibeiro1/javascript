a5ccf1e0abbd09317d5de629d3e5404f831d64f4
julioribeiro1 <juliodevstudent@gmail.com>
2022-05-25 10:23:30 -0300

    True x False

---
+++ Condicões/True_false.js
@@ -0,0 +1,17 @@
+ /*function nad(a,b){
+    if (a === b){
+        return false
+    } else{
+        return true
+    }
+ }
+ console.log(nad(true,true))
+*/
+ function nad(a,b){
+    if (a != b){
+    return true
+} else{
+    return false
+} 
+}
+console.log(nad(3,8))
