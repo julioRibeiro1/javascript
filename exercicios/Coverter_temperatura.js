3fed732c1e3e90cfbac680e27c2086850bb5ba78
julioribeiro1 <juliodevstudent@gmail.com>
2022-05-30 19:24:43 -0300

    ao contrario

--- exercicios/Coverter_temperatura.js
+++ exercicios/Coverter_temperatura.js
@@ -1,5 +1,13 @@
-function celsiusParaFahrenheit(n){
+/*function celsiusParaFahrenheit(n){
     return n * 1.8+32
 }
 let r = celsiusParaFahrenheit(32)
+console.log (r)
+*/
+
+
+function fahrenheitParaCelsius(n){
+    return (n-32) /1.8
+}
+let r = fahrenheitParaCelsius(89.7)
 console.log (r)
