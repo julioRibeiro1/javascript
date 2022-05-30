ff18348ca85d53acebd808fb858aeb3f9ca32726
julioribeiro1 <juliodevstudent@gmail.com>
2022-05-30 18:18:00 -0300

    exclusão

--- exe.array/array_2D.js
+++ exe.array/array_2D.js
@@ -1,4 +1,4 @@
-let arrayMultidimencional = [
+/*let arrayMultidimencional = [
 ['Trabalho', 6],
 ['Comer',2 ],
 ['Video Game', 4],
@@ -9,4 +9,9 @@ let arrayMultidimencional = [
 
 for (let i = 0; i < arrayMultidimencional.length; i++){
     console.log(arrayMultidimencional[i])
+}*/
+
+function square(numero){
+    return numero *numero
 }
+console.log(square(2))
